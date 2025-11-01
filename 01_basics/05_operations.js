d70f12508e05205bbd273279058e388acc68b88d
Chhotu kumar <110243856+TechWithChhotu@users.noreply.github.com>
2025-11-01 02:30:18 +0000

    operations

---
+++ 01_basics/05_operations.js
@@ -0,0 +1,36 @@
+// **************Basic operations**************
+let x = 2;
+let y = 3;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x*y);
+// console.log(y/x);
+// console.log(y%x);
+
+
+// *********** Operation with string & number ***************
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+// console.log(1+"Chhotu");
+// console.log("Chhotu "+"Patel");
+
+// **************** Increment Operator ***************
+
+let a =5;
+let b = a++;//1st assign op performed and then increment.
+console.log(`a:${a} | b:${b}`);
+
+b = ++a;//1st increment op performed and then assign
+console.log(`a:${a} | b:${b}`);
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Complete JavaScript.
 [variables](./01_basics/02_variables.js)
 [datatypes](./01_basics/03_dataTypes.js)
 [datatype_conversion](./01_basics/04_datatypeConversion.js)
+[operations](./01_basics/05_operations.js)
