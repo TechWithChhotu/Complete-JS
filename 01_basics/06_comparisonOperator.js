e53af681246fbddcf9c0782e7e7aabf2ec81c35c
Chhotu kumar <110243856+TechWithChhotu@users.noreply.github.com>
2025-11-02 01:51:45 +0000

    comparison operators

---
+++ 01_basics/06_comparisonOperator.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2<=2);
+// console.log(5==4);
+// console.log(5==5);
+// console.log(5!=4);
+
+
+// console.log(null<0);
+// console.log(null<=0);
+// console.log(null==0);//no conversion, special rule for null; null not equal to any number even 0
+// console.log(null>0);
+// console.log(null>=0);
+
+
+// console.log(null==undefined);// Conversion happened.
+console.log(null===undefined);//conversion not happened due to strict equality check
+
+
+
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Complete-JS
 Complete JavaScript.
 
-[variables](./01_basics/02_variables.js)
-[datatypes](./01_basics/03_dataTypes.js)
-[datatype_conversion](./01_basics/04_datatypeConversion.js)
+[variables](./01_basics/02_variables.js)  
+
+[datatypes](./01_basics/03_dataTypes.js)  
+
+[datatype_conversion](./01_basics/04_datatypeConversion.js) 
+
 [operations](./01_basics/05_operations.js)
+
+[comparison_Operators](./01_basics/06_comparisonOperator.js)
